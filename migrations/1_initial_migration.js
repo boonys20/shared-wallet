6c392f8f48e071febe296a452ab3d586c49864f1
Kung Devz <kungcs20@gmail.com>
2021-08-05 15:27:01 +0700

    update readme and sharewallet code

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+//const Migrations = artifacts.require("Migrations");
 const SharedWallet = artifacts.require("SharedWallet")
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  //deployer.deploy(Migrations);
   deployer.deploy(SharedWallet);
 };
