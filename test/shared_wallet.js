cd2d1b98985c9cefd42beb822a5e18dca0c49804
Kung Devz <kungcs20@gmail.com>
2021-08-20 02:25:38 +0700

    add frontend modules

--- test/shared_wallet.js
+++ test/shared_wallet.js
@@ -5,10 +5,21 @@ const SharedWallet = artifacts.require("SharedWallet");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("SharedWallet", function (/* accounts */) {
-  it("should assert true", async function () {
+contract("SharedWallet", accounts => {
+
+  it("should status of deployed is true", async function () {
     await SharedWallet.deployed();
     return assert.isTrue(true);
   });
+
+  it("should not emptry the first account", () => {
+    SharedWallet.deployed().then(instance => instance.getBalance.call(accounts[0])).then(balance => {
+      assert.equal(
+        balance.valueOf(),
+        balance.valueOf(),
+        "balance in first account is " + balance.valueOf()
+      );
+    })});
+  
 
 });
